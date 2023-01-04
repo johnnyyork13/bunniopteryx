290953a97f8a84056435a6687620d3d46aa8740c
johnnyyork13 <johnnyyork13@hotmail.com>
2023-01-03 19:29:12 -0500

    add page links and more styling

--- public/javascript.js
+++ public/javascript.js
@@ -2,35 +2,53 @@ const leftBtn = document.getElementById('artLeft');
 const viewBtn = document.getElementById('artView');
 const rightBtn = document.getElementById('artRight');
 const btnList = document.getElementsByClassName('navButton');
-const currentSlide = document.getElementById('art');
 
+const artArray = document.getElementsByClassName('art');
 
-let slideNum = 1;
-
+let numSlide = 1;
+let left = false;
+let right = false;
 
 leftBtn.addEventListener('click', function(){
-    if (slideNum > 1)
-    {
-        slideNum -= 1;
-    } else {
-        slideNum = 8;
-    }
+    left = true;
 
-    updateSlide();
+    changeSlide();
 })
 
 rightBtn.addEventListener('click', function(){
-    if (slideNum < 8)
-    {
-        slideNum += 1;
-    } else {
-        slideNum = 1;
-    }
+    left = false;
 
-    updateSlide();
+    changeSlide();
 })
 
-function updateSlide()
+function changeSlide()
 {
-    currentSlide.src = `images/artwork${slideNum}.jpg`;
+    console.log(left, numSlide);
+    if (left && numSlide > 1)  {
+        console.log('one')
+        numSlide -= 1;
+    } else if (left && numSlide == 1) {
+        console.log('two')
+        numSlide = 8;
+    } else if (!left && numSlide < 8) {
+        console.log('three')
+        numSlide += 1;
+    } else if (!left && numSlide == 8) {
+        console.log('four')
+        numSlide = 1;
+    }
+
+    for (i = 0; i < artArray.length; i++) {
+        let e = artArray[i];
+        if (e.id === `art${numSlide}`)
+        {
+            console.log(e.id, numSlide);
+            e.style.visibility = 'visible';
+            e.style.opacity = '1';
+        } else {
+            e.style.visibility = 'hidden';
+            e.style.opacity = '0';
+        }
+    }
 }
+
